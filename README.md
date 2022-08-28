9aaddd3962cbae9f74768c670ade282f655bfd2f
mametarou967 <mametarou967@gmail.com>
2022-08-28 23:31:16 +0900

    playコマンドの追加

--- README.md
+++ README.md
@@ -22,3 +22,21 @@
 * SoftwareSerial
 * MP3Player_KT403A.h
     * 公式ではないため[github](https://github.com/lemairec/rasika/blob/master/lib/MP3Player_KT403A/MP3Player_KT403A.h)からダウンロードし、ローカルに保存のこと
+
+## 使用方法
+
+### ホストPCの設定
+
+* ボーレート:115200
+* 改行コード:\r\n(=CR+LF)
+
+### テラタームで使用する際の注意点
+
+* エコーバックがないため注意すること
+    * （Seeeduino Xiao側の青色LEDを頼りに送信されていることを確認する)
+
+### コマンド
+
+| コマンド | 変数 | 機能 | 詳細 |
+| :--- | :--- | :--- | :--- |
+| AT+PLAY=? | 1～N | 音声再生 | ?で指定した曲番号を生成する |
--- WaveModuleWithSeeeduinoXiao.ino
+++ WaveModuleWithSeeeduinoXiao.ino
@@ -1,17 +1,40 @@
 #include <SoftwareSerial.h>
 #include "MP3Player_KT403A.h"
+#include "ATCommands.h"
 
+// grove mp通信用
 SoftwareSerial mp3(7, 6);
 
+// host pcとのシリアル通信用
+#define WORKING_BUFFER_SIZE 255 // The size of the working buffer (ie: the expected length of the input string)
+// declare the commands in an array to be passed during initialization
+ATCommands AT; // create an instance of the class
+
+bool at_write_cmd_play(ATCommands *sender)
+{
+    String str1 = sender->next();
+    int playNumber = str1.toInt();
+    SpecifyMusicPlay(playNumber);
+    return true; // tells ATCommands to print OK
+}
+
+static at_command_t commands[] = {
+    {"+PLAY", NULL, NULL, NULL, at_write_cmd_print},
+};
+
 void setup() {
+  // grove mp通信用
   mp3.begin(9600);          // grove mp3用のシリアル通信を開始
   delay(100);               // 一拍置く
   SelectPlayerDevice(0x02); // mp3モジュールをSDカードから読み取るように設定
   SetVolume(0x1E);          // ボリュームをマックスに指定
+
+  // host pcとのシリアル通信用
+  Serial.begin(115200);
+  AT.begin(&Serial, commands, sizeof(commands), WORKING_BUFFER_SIZE);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-    SpecifyMusicPlay(1);
-    delay(10000);
+  AT.update();
 }
